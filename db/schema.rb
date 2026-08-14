7c734a0589d2e2392003bee4dd8cbcb34889f231
kanzaki-git <ashpem6mp@gmail.com>
2026-08-14 13:44:43 +0900

    AIシナリオの生成回数を制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_07_28_195621) do
+ActiveRecord::Schema[8.0].define(version: 2026_08_14_031508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -127,6 +127,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_07_28_195621) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "scenario_generation_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
