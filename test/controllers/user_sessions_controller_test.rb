2b62a571e4ee9e39775d7d75a7cc3e03aca95b97
kanzaki-git <ashpem6mp@gmail.com>
2026-08-31 03:48:40 +0900

    ログイン失敗時のメッセージを表示

--- test/controllers/user_sessions_controller_test.rb
+++ test/controllers/user_sessions_controller_test.rb
@@ -1,8 +1,28 @@
 require "test_helper"
 
 class UserSessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  setup do
+    @user = User.create!(
+      name: "ログインテストユーザー",
+      email: "login-test@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+  end
+
+  test "ログイン画面を表示できる" do
     get login_url
+
     assert_response :success
+  end
+
+  test "誤ったパスワードの場合はエラーメッセージを表示する" do
+    post login_url, params: {
+      email: @user.email,
+      password: "wrong-password"
+    }
+
+    assert_response :unprocessable_entity
+    assert_select ".alert-danger", text: "ログインに失敗しました"
   end
 end
