0bf257ca7fb49e17dfa1758eebee3c6a51e44d4c
kanzaki-git <ashpem6mp@gmail.com>
2026-08-31 02:54:53 +0900

    Dockerビルド時のAPP_HOST未設定エラーを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {
-    host: ENV.fetch("APP_HOST"),
+    host: ENV.fetch("APP_HOST", "localhost"),
     protocol: "https"
   }
 
